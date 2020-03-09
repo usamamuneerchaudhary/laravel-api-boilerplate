a21c2b77fff41bd46263947ad34ec67ca58ec307
usamamuneerchaudhary <info@usamamuneer.me>
2020-03-09 12:45:31 +0500

    updated for laravel v7,

--- config/app.php
+++ config/app.php
@@ -165,8 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-
-        Barryvdh\Cors\ServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
         Akaunting\Firewall\Provider::class,
 
